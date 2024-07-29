62d2fe6cf56bf0dda2e5cae4632867d0f835ad2a
Nusnan .A.M <82087528+mohamednusnan@users.noreply.github.com>
2024-07-29 15:32:22 +0530

    2nd data

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,24 @@
-const express = require('express')
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
-app.listen("4000",() =>{
-        console.log("listening to 4000");
+app.get("/",(req,res) => {
+    res.send("Hello ");
 });
+
+//middleware
+app.use((req,res,next) =>{
+    console.log('path' + req.path +'method' + req.method);
+    next();
+});
+
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+        app.listen(process.env.PORT,() =>{
+            console.log("DB Connected  " + process.env.PORT);
+    });
+ }).catch((error)=>console.log(error));
+
+
